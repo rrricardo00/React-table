458a634c38a75d32d8a60cdb22273164e83413d6
rrricardo00 <rrricardo00@yahoo.com.br>
2021-11-09 03:36:18 -0300

    react

--- src/Table.js
+++ src/Table.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Table = () => {
 
     //variables
+    const tds = document.querySelectorAll('tbody tr')
     const [dados, setDados] = React.useState(null)
     const [loading, setLoading] = React.useState(null)
     const [input, setInput] = React.useState({
@@ -21,14 +22,28 @@ const Table = () => {
         const json = await url.json()
         setDados(json)
         setLoading(true)
+
     }, [dados])
 
 
     //input
-    const handleChange = ({target}) =>{
-        const {id, value} = target
-        setInput({...input, [id]: value})
-        console.log(target)
+    const handleChange = ({ target }) => {
+        const { id, value } = target
+        setInput({ ...input, [id]: value })
+
+    }
+
+    //filter
+    const handleFilter = ({target}) => {
+    
+        for (let index = 0; index <= tds.length - 1; index++) {
+            Array.from(tds[index].childNodes).filter((item) => {
+                if (item.innerText === target.value) return  tds[index].style.display = ""
+                else return tds[index].style.display = "none"
+            })
+
+        }
+
     }
 
     //load fetch
@@ -39,34 +54,34 @@ const Table = () => {
     if (loading === false) return <p>Loading Data</p>
     if (dados === null) return null
     return (
+
         <div className="table-container">
-           <table>
-               <thead>
-                <tr>
-                    <th>Email<br/><input type="text" id="email" value={input.email} onChange={handleChange}/></th>
-                    <th>First Name<input type="text" id="nome" value={input.nome} onChange={handleChange}/></th>
-                    <th>Last Name<input type="text" id="sobrenome" value={input.sobrenome} onChange={handleChange}/></th>
-                    <th>Primary Group<input type="text" id="grupo" value={input.grupo} onChange={handleChange}/></th>
-                    <th>Phone Number<input type="text" id="telefone" value={input.telefone} onChange={handleChange}/></th>
-                    <th>Hours Studied<input type="text" id="horasEstudadas" value={input.horasEstudadas} onChange={handleChange}/></th>
-                </tr>
-               </thead>
-               <tbody>
-                    
-                        {dados.map((item, index)=>{
-                            return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Email<br /><input type="text" id="email" value={input.email} onChange={handleChange} onKeyUp={handleFilter} /></th>
+                        <th>First Name<input type="text" id="nome" value={input.nome} onChange={handleChange} onKeyUp={handleFilter}/></th>
+                        <th>Last Name<input type="text" id="sobrenome" value={input.sobrenome} onChange={handleChange} /></th>
+                        <th>Primary Group<input type="text" id="grupo" value={input.grupo} onChange={handleChange} /></th>
+                        <th>Phone Number<input type="text" id="telefone" value={input.telefone} onChange={handleChange} /></th>
+                        <th>Hours Studied<input type="text" id="horasEstudadas" value={input.horasEstudadas} onChange={handleChange} /></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dados.map((item, index) => {
+                        return (
                             <tr key={index}>
-                            <td>{item.email}</td>
-                            <td>{item.nome}</td>
-                            <td>{item.sobrenome}</td>
-                            <td>{item.grupo}</td>
-                            <td>{item.telefone}</td>
-                            <td>{item.estudadas}</td>
+                                <td>{item.email}</td>
+                                <td>{item.nome}</td>
+                                <td>{item.sobrenome}</td>
+                                <td>{item.grupo}</td>
+                                <td>{item.telefone}</td>
+                                <td>{item.estudadas}</td>
                             </tr>
-                            )
-                        })}
-               </tbody>
-           </table>
+                        )
+                    })}
+                </tbody>
+            </table>
 
         </div>
     )
